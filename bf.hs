10a815037f4b5f89a7cfaedb4763a730d4a7ddcf
Paul Tilley <t1000.pixlark@gmail.com>
2019-01-04 03:02:25 -0500

    Input and output operations

--- bf.hs
+++ bf.hs
@@ -1,13 +1,18 @@
 import Data.Maybe
 import Data.Word
+import Data.Char
+import System.IO
+import System.Environment
+import System.Exit
 
-data Command = NoOperation
-             | Increment
+data Command = Increment
              | Decrement
              | MoveRight
              | MoveLeft
              | StartLoop
              | EndLoop
+             | WriteOut
+             | ReadIn
   deriving Show
 
 assocCommand :: Char -> Maybe Command
@@ -18,6 +23,8 @@ assocCommand n = case n of
   '<' -> Just MoveLeft
   '[' -> Just StartLoop
   ']' -> Just EndLoop
+  '.' -> Just WriteOut
+  ',' -> Just ReadIn
   _   -> Nothing
 
 parseSource :: [Char] -> [Command]
@@ -46,65 +53,57 @@ firstEnd counter cmds = firstEndHelper counter cmds 0
                        else firstEndHelper (counter + 1) cmds (depth - 1)
           _         -> firstEndHelper (counter + 1) cmds depth
 
-interpretCommand :: Command -> BFState -> BFState
+interpretCommand :: Command -> BFState -> IO BFState
 interpretCommand command (BFState cmds cursor memory counter loops) =
   case command of
-    Increment -> BFState
-                 cmds
-                 cursor
-                 (changeNth
-                   memory
-                   (memory !! fromIntegral cursor + 1)
-                   cursor)
-                 (counter + 1)
-                 loops
-    Decrement -> BFState
-                 cmds
-                 cursor
-                 (changeNth
-                   memory
-                   (memory !! fromIntegral cursor - 1)
-                   cursor)
-                 (counter + 1)
-                 loops
-    MoveRight -> BFState
-                 cmds
-                 (cursor + 1)
-                 memory
-                 (counter + 1)
-                 loops
-    MoveLeft  -> BFState
-                 cmds
-                 (cursor - 1)
-                 memory
-                 (counter + 1)
-                 loops
-    StartLoop -> if (memory !! (fromIntegral cursor)) == 0
-                 then BFState
-                      cmds
-                      cursor
-                      memory
-                      (firstEnd (counter + 1) cmds)
-                      loops
-                 else BFState
-                      cmds
-                      cursor
-                      memory
-                      (counter + 1)
-                      (counter : loops)
-    EndLoop   -> BFState
-                 cmds
-                 cursor
-                 memory
-                 (head loops)
-                 (tail loops)
+    Increment -> return $ BFState cmds cursor
+                 (changeNth memory
+                   (memory !! fromIntegral cursor + 1) cursor)
+                 (counter + 1) loops
+                 
+    Decrement -> return $ BFState cmds cursor
+                 (changeNth memory
+                   (memory !! fromIntegral cursor - 1) cursor)
+                 (counter + 1) loops
+                 
+    MoveRight -> return $ BFState cmds (cursor + 1) memory (counter + 1) loops
+    
+    MoveLeft  -> return $ BFState cmds (cursor - 1) memory (counter + 1) loops
+    
+    StartLoop -> return $
+                 if (memory !! (fromIntegral cursor)) == 0
+                 then BFState cmds cursor memory
+                      (firstEnd (counter + 1) cmds) loops
+                 else BFState cmds cursor memory (counter + 1) (counter : loops)
+                 
+    EndLoop   -> return $ BFState cmds cursor memory (head loops) (tail loops)
+    
+    WriteOut  -> (putStr $ [chr $ fromIntegral $ memory !! (fromIntegral cursor)]) >>
+                 (return $ BFState cmds cursor memory (counter + 1) loops)
 
-bfFold :: [Command] -> BFState
-bfFold commands = bfLoop (BFState commands 0 (replicate 10 0) 0 [])
+    ReadIn    -> do eof <- isEOF
+                    if eof
+                      then (return $ BFState cmds cursor memory (counter + 1) loops)
+                      else (do c <- getChar
+                               (return $ BFState cmds cursor
+                                (changeNth memory (fromIntegral $ ord c) cursor) (counter + 1) loops))
+
+bfFold :: [Command] -> IO BFState
+bfFold commands = bfLoop (BFState commands 0 (replicate 30000 0) 0 [])
   where bfLoop state = if (counter state) >= (toInteger $ length $ cmds state)
-                       then state
-                       else bfLoop (interpretCommand ((cmds state) !! (fromIntegral $ counter state)) state)
+                       then (return state)
+                       else (interpretCommand
+                             ((cmds state) !! (fromIntegral $ counter state))
+                             state) >>= bfLoop
+
+parseArgs :: [String] -> IO String
+parseArgs [name] = return name
+parseArgs _ = do
+  putStrLn("Provide one brainfuck file to interpret")
+  exitWith $ ExitFailure 1
 
 main = do
-  source <- getContents
-  putStr $ show $ bfFold $ parseSource source
+  filename <- getArgs >>= parseArgs
+  withFile filename ReadMode $ \handle -> do
+    source <- hGetContents handle
+    bfFold $ parseSource source
